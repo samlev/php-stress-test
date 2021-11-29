525edd9541c84098c69c25b8c54d80607a98ecd5
Samuel Levy <sam@samuellevy.com>
2021-11-29 21:19:46 +1000

    Added length test

---
+++ test-length.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * test-length.php
+ *
+ * Tests for the maximum length of a PHP file by brute force.
+ *
+ * @package
+ * @author: Samuel Levy <sam@determineddevelopment.com>
+ */
+
+$fname = __DIR__ . '/out/length.php';
+$php = 'php8.1 -d memory_limit=128M';
+$step = 1048576;
+
+for ($rounds = 1 ;; $rounds ++) {
+    $fp = fopen($fname, 'w');
+    fwrite($fp, '<?php $a = "');
+    for ($r = 1; $r <= $rounds; $r ++) {
+        fwrite($fp, str_repeat('a', $step));
+    }
+    fwrite($fp, '"; echo "done " . strlen($a);');
+    fclose($fp);
+
+    $out = shell_exec($php . ' ' . $fname);
+
+    if (! str_starts_with($out, 'done')) {
+        echo 'Failed at ' . ($rounds * $step) . ' characters' . "\n";
+        break;
+    }
+}
+
+echo 'Completed testing!' . "\n";
